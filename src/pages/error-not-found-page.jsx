d5569dd336f36b201400b2a9bf6ceb63cd900451
Camilo <camilo.acosta97@gmail.com>
2024-05-07 20:30:28 -0500

    chore: Update npm dependencies and add Next UI components

--- src/pages/error-not-found-page.jsx
+++ src/pages/error-not-found-page.jsx
@@ -1,7 +1,10 @@
-import { Link } from "react-router-dom";
-import { Button } from "../features/ui";
+// Router
+import { useNavigate } from "react-router-dom";
+//Next Ui Components
+import { Button } from "@nextui-org/react";
 
 export default function ErrorNotFoundPage() {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen flex items-center justify-center   px-4 sm:px-6 lg:px-8">
       <div className="space-y-4 text-center">
@@ -11,13 +14,9 @@ export default function ErrorNotFoundPage() {
         <p className="text-sm sm:text-base md:text-lg text-gray-700 ">
           Lo sentimos, la página que buscas no existe.
         </p>
-        <Button
-          label={
-            <Link className="text-white" to="/">
-              Volver al inicio
-            </Link>
-          }
-        />
+        <Button onClick={() => navigate("/")} color="primary">
+          Volver al inicio
+        </Button>
       </div>
     </div>
   );
